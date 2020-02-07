892b4901c7b4601741d65678d620d6110bd5a440
ElderSam <eldersamuel98@gmail.com>
2020-02-06 21:34:11 -0300

    configurando acesso ao Banco de Dados

--- application/controllers/Restrict.php
+++ application/controllers/Restrict.php
@@ -1,10 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+
 class Restrict extends CI_Controller{
 
     public function index(){
-        $this->template->show("restrict.php");
+
+       $this->template->show("restrict.php");
     }
 }
 
